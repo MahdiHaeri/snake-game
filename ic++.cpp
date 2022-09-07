5a4d5f2731b72ce283ec3d1701e2b435e7d71dcc
mohammad_mahdi_haeri <mahdihaerim@gmail.com>
2022-09-07 14:35:00 +0430

    add getch() and kbhit() function in my project

--- ic++.cpp
+++ ic++.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
-#include <limits>
+#include <termios.h>
+#include <unistd.h>
 
 using namespace std;
 
+
+#include <sys/ioctl.h>
+
+bool kbhit()
+{
+    termios term;
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+
+
+/* reads from keypress, doesn't echo */
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+
+
 int main(int argc, char const *argv[]) {
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  char x;
+  while (true) {
+    if (kbhit()) {
+      x = getch();
+      cout << x;
+      break;
+    }
+    cout << "hello" << endl;
+  }
   return 0;
 }
--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,9 @@
 #include <queue>
 #include <random>
 #include <time.h>       /* time */
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 
@@ -21,9 +24,44 @@ class Food;
 class Snake;
 class Map;
 
+// ----------------------------------- additional function -----------------------------
+
 void gotoxy(int x, int y) {
   printf("%c[%d;%df", 0x1B, y, x);
 }
+
+bool kbhit()
+{
+    termios term;
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+
+/* reads from keypress, doesn't echo */
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+//------------------------------------------------------------------------------
 
 
 class Map {
