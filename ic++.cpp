0f9aa801e4c46cdb09381ded6b8b82af0260bd39
mohammad_mahdi_haeri <mahdihaerim@gmail.com>
2022-09-07 13:28:32 +0430

    upgrade snake project

--- ic++.cpp
+++ ic++.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <time.h>
+#include <limits>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
   return 0;
 }
