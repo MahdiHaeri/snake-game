edb64aa550272237af502f53b0857e4246f645ca
mohammad_mahdi_haeri <mahdihaerim@gmail.com>
2022-12-06 15:07:40 +0330

    add define of colors code

--- snake.cpp
+++ snake.cpp
@@ -27,6 +27,25 @@ class Food;
 class Snake;
 class Map;
 
+//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#define __RESET   "\033[0m"
+#define __BLACK   "\033[30m"      /* Black */
+#define __RED     "\033[31m"      /* Red */
+#define __GREEN   "\033[32m"      /* Green */
+#define __YELLOW  "\033[33m"      /* Yellow */
+#define __BLUE    "\033[34m"      /* Blue */
+#define __MAGENTA "\033[35m"      /* Magenta */
+#define __CYAN    "\033[36m"      /* Cyan */
+#define __WHITE   "\033[37m"      /* White */
+#define __BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define __BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define __BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define __BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define __BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define __BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define __BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define __BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 // ----------------------------------- additional function -----------------------------
 
 void gotoxy(int x, int y) {
